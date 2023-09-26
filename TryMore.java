076ebb8f559e85d960152d1633be020230f8ee7d
salmaazzz <azizahsalma2304@gmail.com>
2023-09-26 17:36:20 +0700

    Initial commit

---
+++ TryMore.java
@@ -0,0 +1,8 @@
+public class TryMore {
+
+    public static void main (String[] args) {
+        System.out.println("Name: Salma Nur Azizah");
+        System.out.println("NIM: 607062300040");
+        System.out.println("Kelas: D3-IF-47-03");
+    }
+}
